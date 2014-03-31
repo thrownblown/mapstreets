6a8bd65dfd14d454c4c02caa5fb805a8eb63566d
Hack Reactor Students <accounts@hackreactor.com>
2014-03-31 12:06:56 -0700

    corner action right

--- streetmap.js
+++ streetmap.js
@@ -1,18 +1,35 @@
- 
+
 var map;
 var myPoly;
 var path = [
-new google.maps.LatLng(37.7107,-122.4376),
-new google.maps.LatLng(37.7147,-122.4396),
-new google.maps.LatLng(37.7087,-122.4366)
-]
+new google.maps.LatLng(37.78371,-122.40),
+new google.maps.LatLng(37.78370618798191,-122.408766746521),
+new google.maps.LatLng(37.79,-122.41)
+];
+
+var testLat = [];
+var testLing = [];
+//moding our big A Array to provide easy min/max methods
+Array.max = function( array ){
+  return Math.max.apply( Math, array );
+};
+
+Array.min = function( array ){
+  return Math.min.apply( Math, array );
+};
+
+var seCorner;
+var neCorner;
+var swCorner;
+var nwCorner;
 
 function initialize() {
   var mapOptions = {
-    center: new google.maps.LatLng(37.7107,-122.4376),
-    zoom: 15
+    center: new google.maps.LatLng(37.78370618798191,-122.408766746521),
+    // center: new google.maps.LatLng(37.7107,-122.4376),
+    zoom: 14
   };
-        
+
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
 
@@ -34,14 +51,56 @@ function initialize() {
 function addLatLng(event) {
   path.push(event.latLng);
   myPoly.setPaths(path);
-  console.log(myPoly.getPaths().getArray())
+  console.log(myPoly.getPaths().getArray());
 
-  myArray = myPoly.getPaths().getArray()[0].j
+  var myArray = myPoly.getPaths().getArray()[0].j;
   console.log(myArray);
-  for (i=0; i<myArray.length; i++){
-    
+  for (var i = 0; i < myArray.length; i++){
+    testLat.push(myArray[i].k);
+    testLing.push(myArray[i].A);
+  }
+}
+function getCorners(){
+  if (seCorner){
+    seCorner.setMap(null);
+    neCorner.setMap(null);
+    swCorner.setMap(null);
+    nwCorner.setMap(null);
   }
 
+  seCorner = new google.maps.Marker({
+    position: new google.maps.LatLng(Array.min(testLat),Array.max(testLing)),
+    title: 'seCorner',
+    map: map
+  });
+  console.log('seCorner', Array.min(testLat),Array.max(testLing));
+
+  neCorner = new google.maps.Marker({
+    position: new google.maps.LatLng(Array.max(testLat),Array.max(testLing)),
+    title: 'neCorner',
+    map: map
+  });
+  console.log('neCorner', Array.max(testLat),Array.max(testLing));
+
+  nwCorner = new google.maps.Marker({
+    position: new google.maps.LatLng(Array.max(testLat),Array.min(testLing)),
+    title: 'nwCorner',
+    map: map
+  });
+  console.log('nwCorner', Array.max(testLat),Array.min(testLing));
+
+  swCorner = new google.maps.Marker({
+    position: new google.maps.LatLng(Array.min(testLat),Array.min(testLing)),
+    title: 'swCorner',
+    map: map
+  });
+  console.log('swCroner', Array.min(testLat),Array.min(testLing));
 }
+// $(document).ready(function(){
+//   $("a .showCorners").click(function(){
+//     console.log('yo');
+//     getCorners();
+//   });
+// });
 
 google.maps.event.addDomListener(window, 'load', initialize);
